2aff397ae635f84d5fe7851d9ef6e915c7e20f8b
dapson9898 <83287316+dapson9898@users.noreply.github.com>
2026-01-13 23:06:55 +0100

    added color and sound

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // let userScore = prompt("Kindly enter a number: ")
+let result = document.getElementById("result")
+let grade_sound = document.getElementById("grade_sound")
+let inputBtn = document.querySelector("input")
+
 function gradeCalculator() {
     let userScore = Number(document.getElementById("score").value);
     let grade;
@@ -6,25 +10,38 @@ function gradeCalculator() {
         alert("Score cannot be blank");
         return;
     } else if (userScore < 0) {
-        alert("INVALID! Lesser than zero");
+        alert("INVALID! Grade can not be lesser than zero");
         return;
     } else if (userScore < 40) {
         grade = "F";
+        result.style.color = "red"
+        grade_sound.src = "./applause.mp3"
     } else if (userScore < 45) {
         grade = "E";
+        result.style.color = "orange"
+        grade_sound.src = "./applause.mp3"
     } else if (userScore < 50) {
         grade = "D";
+        result.style.color = "pink"
+        grade_sound.src = "./applause.mp3"
     } else if (userScore < 60) {
         grade = "C";
+        result.style.color = "yellow"
+        grade_sound.src = "./applause.mp3"
     } else if (userScore < 70) {
         grade = "B";
+        result.style.color = "blue"
+        grade_sound.src = "./applause.mp3"
     } else if (userScore <= 100) {
         grade = "A";
+        result.style.color = "green"
+        grade_sound.src = "./applause.mp3"
     } else if (userScore > 100) {
-        alert("INVALID! Greater than 100");
+        alert("INVALID! Grade can not be greater than 100");
         return;
     } else {
         alert("Invalid Input");
     }
-    result.innerText = "Your grade is " + grade;
+    result.innerText = `Your grade is "${grade}".`;
 }
+inputBtn.addEventListener("keypress", event => (event.key === "Enter"? gradeCalculator(): null ))
